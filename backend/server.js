94cb7b1a24e311980a2fc02443f6e1d92112e413
Batu Aybak <48447901+batuAybak@users.noreply.github.com>
2025-09-10 14:53:48 -0400

    update port to 3000

--- backend/server.js
+++ backend/server.js
@@ -25,5 +25,5 @@ app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => {
     res.send('API Running');
 });
-const PORT = process.env.PORT || 5000;
+const PORT = 3000// process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
